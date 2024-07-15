e75e586380245568bb05cc3f4611f41c18cba3fe
Walter <walterwong1110@outlook.com>
2024-07-15 17:15:31 +0800

    优化ID生成逻辑

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-Snowflake
-=========
+# Snowflake
 
 Snowflake is a distributed unique ID generator inspired by [Twitter's Snowflake](https://blog.twitter.com/2010/announcing-snowflake).
 
 A Snowflake ID is composed of
 
-    41 bits for time in units of 1 msec
-    10 bits for a work id
-    12 bits for a sequence number
+* time - 41 bits (millisecond precision w/ a custom epoch gives us 69 years)
+* configured machine id - 10 bits - gives us up to 1024 machines
+* sequence number - 12 bits - rolls over every 4096 per machine (with protection to avoid rollover in the same ms)
 
-The start time of the Snowflake is set to "2024-07-15 00:00:00 +0000 UTC".
+By the way, the start time of the Snowflake is set to "2024-07-15 00:00:00 +0000 UTC".
 
-Installation
-------------
+## System Clock Dependency
+
+You should use NTP to keep your system clock accurate.  Snowflake protects from non-monotonic clocks, i.e. clocks that run backwards.  If your clock is running fast and NTP tells it to repeat a few milliseconds, snowflake will refuse to generate ids(return 0) until a time that is after the last time we generated an id. Even better, run in a mode where ntp won't move the clock backwards. See http://wiki.dovecot.org/TimeMovedBackwards#Time_synchronization for tips on how to do this.
+
+## Installation
+
 ```
 go get github.com/walterwong1001/snowflake
 ```
 
-Usage
------
+## Usage
 ```go
 s, err := snowflake.NewSnowflake(1)
 if err != nil {
--- pkg/snowflake/snowflake.go
+++ pkg/snowflake/snowflake.go
@@ -2,35 +2,35 @@ package snowflake
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"sync"
 	"time"
 )
 
 const (
-	WORK_ID_BITS  = 10                         // bit length of work id
-	SEQUENCE_BITS = 12                         // bit length of sequence number
-	MAX_WORK_ID   = -1 ^ (-1 << WORK_ID_BITS)  // max work id (1023)
-	SEQUENCE_MASK = -1 ^ (-1 << SEQUENCE_BITS) // sequence mask 4095
-	EPOCH         = 1721001600000              // 2024-07-15 00:00:00 UTC in milliseconds
+	MACHINE_ID_BITS = 10                           // bit length of machine id
+	SEQUENCE_BITS   = 12                           // bit length of sequence number
+	MAX_MACHINE_ID  = -1 ^ (-1 << MACHINE_ID_BITS) // max machine id (1023)
+	SEQUENCE_MASK   = -1 ^ (-1 << SEQUENCE_BITS)   // sequence mask 4095
+	EPOCH           = 1721001600000                // 2024-07-15 00:00:00 UTC in milliseconds
 )
 
 type snowflake struct {
-	mutex    sync.Mutex
-	epoch    int64
-	workId   uint16
-	sequence uint64
-	elapsed  int64
+	mutex         sync.Mutex
+	machineId     uint16
+	sequence      uint64
+	lastTimestamp int64
 }
 
-func NewSnowflake(workId uint16) (*snowflake, error) {
-	if workId > MAX_WORK_ID {
-		return nil, errors.New("workId exceeds max value")
+func NewSnowflake(machineId uint16) (*snowflake, error) {
+	if machineId > MAX_MACHINE_ID {
+		return nil, errors.New(fmt.Sprintf("Machine Id can't be greater than %d", MAX_MACHINE_ID))
 	}
 	return &snowflake{
-		epoch:    EPOCH,
-		workId:   workId,
-		sequence: 0,
-		elapsed:  0,
+		machineId:     machineId,
+		sequence:      0,
+		lastTimestamp: 0,
 	}, nil
 }
 
@@ -38,24 +38,31 @@ func (s *snowflake) NextID() uint64 {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	currentMilli := time.Now().UnixNano() / 1000000 // 转换为毫秒
-	// 当前时间相对于epoch的毫秒数
-	elapsedMilli := currentMilli - s.epoch
-	if s.elapsed < elapsedMilli {
-		s.elapsed = elapsedMilli
-		s.sequence = 0
-	} else {
-		s.sequence = (s.sequence + 1) & SEQUENCE_MASK
-		// 一个毫秒周期内，sequence已到达最大值，则等待下一个毫秒周期
-		if s.sequence == 0 {
-			s.elapsed++
-			// 等待下一毫秒
-			for elapsedMilli <= s.elapsed {
-				elapsedMilli = (time.Now().UnixNano() / 1000000) - s.epoch
-			}
-		}
+	timestamp := time.Now().UnixMilli() // 转换为毫秒
+	if timestamp < s.lastTimestamp {
+		log.Printf("clock is moving backwards.  Rejecting requests until %d.\n", s.lastTimestamp)
+		return 0
 	}
 
-	return uint64(currentMilli-s.epoch)<<(WORK_ID_BITS+SEQUENCE_BITS) | uint64(s.workId)<<SEQUENCE_BITS | s.sequence
+	if s.lastTimestamp == timestamp {
+		s.sequence = (s.sequence + 1) & SEQUENCE_MASK
+		if s.sequence == 0 {
+			timestamp = tilNextMillis(timestamp)
+		}
+	} else {
+		s.sequence = 0
+	}
 
+	s.lastTimestamp = timestamp
+
+	return uint64(timestamp-EPOCH)<<(MACHINE_ID_BITS+SEQUENCE_BITS) | uint64(s.machineId)<<SEQUENCE_BITS | s.sequence
+
+}
+
+func tilNextMillis(timestamp int64) int64 {
+	var current = time.Now().UnixMilli()
+	for current <= timestamp {
+		current = time.Now().UnixMilli()
+	}
+	return current
 }
