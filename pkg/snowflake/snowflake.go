9a16ff7a006fc1a6e1516bb646eb07bbcae12d8d
Walter <walterwong1110@outlook.com>
2024-07-15 22:35:18 +0800

    导出Snowflake

--- pkg/snowflake/snowflake.go
+++ pkg/snowflake/snowflake.go
@@ -8,6 +8,9 @@ import (
 	"time"
 )
 
+// time - 41 bits (millisecond precision w/ a custom epoch gives us 69 years)
+// configured machine id - 10 bits - gives us up to 1024 machines
+// sequence number - 12 bits - rolls over every 4096 per machine (with protection to avoid rollover in the same ms)
 const (
 	MACHINE_ID_BITS = 10                           // bit length of machine id
 	SEQUENCE_BITS   = 12                           // bit length of sequence number
